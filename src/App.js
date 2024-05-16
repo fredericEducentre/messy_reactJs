012e1765f82b98a11fe950995f064a3254b2e1e1
Frédéric Bui <bui.frederic.pro@gmail.com>
2024-05-16 16:10:47 +0200

    add error

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  //Hello
+  //Hello
+  alert("Unexpected Condition");
+  const x = 42;
+  await x;
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ut eleifend est. Phasellus nec nunc gravida, luctus sem sit amet, eleifend nulla. Pellentesque nec risus vehicula, eleifend lectus nec, sagittis velit. In quis nunc efficitur, tempus lorem vel, iaculis elit. Mauris ut accumsan elit, in maximus nulla. Vestibulum tempus nunc quis felis rhoncus imperdiet. Donec nec maximus nisl, in cras.
         </p>
         <a
           className="App-link"
